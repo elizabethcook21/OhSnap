b6bd2d3044ca908af480e207961bd080c3388874
elizabethcook21 <e13cook@gmail.com>
2020-01-31 19:51:03 -0700

    Added two tag functionality

--- MyClinData.R
+++ MyClinData.R
@@ -1,57 +1,32 @@
 library(shiny)
+library(tesseract)
+library(shinyjs)
+library(shinycssloaders)
 
 ui <- fluidPage(
-  
-  # App title ----
-  titlePanel("Hello Shiny!"),
-  
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-      
-      # Input: Slider for the number of bins ----
-      sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-      
-    ),
-    
-    # Main panel for displaying outputs ----
-    mainPanel(
-      
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
-      
-    )
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
+    tags$link(rel = "icon", type = "image/png", href = "Logo.png")
+  ),
+  useShinyjs(),
+  navbarPage(title = "MyClinData", id = 'tabs',
+             tabPanel('Upload Data', value = 'uploadData',
+                      sidebarLayout(
+                        sidebarPanel(h4("Upload Data"),
+                                     p("This is where we will implement Tesseract OCR")), 
+                        mainPanel(p("Here too!")))
+                      ),
+             tabPanel('Graphical Display', value = 'graphicalDisplay',
+                      sidebarLayout(
+                        sidebarPanel(h4("Graphical Display"),
+                                     p("This will be a graphical display")), 
+                        mainPanel(p("Here too!")))
+                      )
   )
 )
 
-
-#ELIZBETH was here and she CAN spell (soemtimes)
 server <- function(input, output) {
   
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
-  output$distPlot <- renderPlot({
-    
-    x    <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    hist(x, breaks = bins, col = "#75AADB", border = "white",
-         xlab = "Waiting time to next eruption (in mins)",
-         main = "Histogram of waiting times")
-    
-  })
 }
   
 shinyApp(ui = ui, server = server)
